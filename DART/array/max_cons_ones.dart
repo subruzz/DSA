a32324312beac2f4543759edb78b61da37ee282e
Subru <subruzz4424@gmail.com>
2024-05-18 00:18:04 +0530

    array continuing

--- DART/array/max_cons_ones.dart
+++ DART/array/max_cons_ones.dart
@@ -1,4 +1,14 @@
 void main() {
-  List<int> list = [1, 1, 0, 1, 1, 1, 0, 1, 1];
-  
+  List<int> list = [1, 1, 1,0, 1, 1, 1,1, 0, 1, 1];
+  int max = 0;
+  int count = 0;
+  for (int i = 0; i < list.length; i++) {
+    if (list[i] == 1) {
+      count++;
+    } else {
+      max = count > max ? count : max;
+      count = 0;
+    }
+  }
+  print('consec 1 is $max');
 }
